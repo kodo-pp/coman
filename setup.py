e97658c32d7f14f51a875851f1fa4d0ff2ab9ce5
Alexander Korzun <sahhash33@gmail.com>
2020-06-27 16:32:31 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name                = 'coman',
+    version             = '1.0.0',
+    description         = 'A simple coroutine manager',
+    author              = 'Alexander Korzun',
+    author_email        = 'sahhash33@gmail.com',
+    license             = 'GPL',
+    packages            = find_packages(),
+)
