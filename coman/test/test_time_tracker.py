eb26f785fa2328499e7200cb1f63be112c7019fa
Alexander Korzun <sahhash33@gmail.com>
2020-07-08 19:53:41 +0300

    Add tests for time tracker

--- coman/test/test_time_tracker.py
+++ coman/test/test_time_tracker.py
@@ -20,3 +20,50 @@ def test_future_time_point_comparison():
         a == e
     with pytest.raises(TypeError):
         a != 1
+
+
+def test_time_tracker_elapsed_time():
+    t = TimeTracker()
+    assert t.elapsed_time() == 0
+    t.update(2)
+    assert t.elapsed_time() == 2
+    t.update(5)
+    assert t.elapsed_time() == 7
+
+
+def test_time_tracker_after():
+    t = TimeTracker()
+    a = t.after(0)
+    b = t.after(5)
+    t.update(3)
+    c = t.after(2)
+    d = t.after(5)
+    t.update(2)
+    e = t.after(0)
+    assert a < b == c == e < d
+
+
+def test_future_time_point_has_passed():
+    t = TimeTracker()
+    a = t.after(0)
+    b = t.after(1)
+    c = t.after(3)
+    assert a.has_passed()
+    assert not b.has_passed()
+    assert not c.has_passed()
+    t.update(1)
+    assert a.has_passed()
+    assert b.has_passed()
+    assert not c.has_passed()
+    t.update(1)
+    assert a.has_passed()
+    assert b.has_passed()
+    assert not c.has_passed()
+    t.update(1)
+    assert a.has_passed()
+    assert b.has_passed()
+    assert c.has_passed()
+    t.update(100)
+    assert a.has_passed()
+    assert b.has_passed()
+    assert c.has_passed()
