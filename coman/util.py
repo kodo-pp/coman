f1248ea007c3306d8af17d3185aff25ee6888962
Alexander Korzun <sahhash33@gmail.com>
2020-07-09 20:24:50 +0300

    Add docs for coman.util

--- coman/util.py
+++ coman/util.py
@@ -1,3 +1,5 @@
+"""A module with some helper functions used internally."""
+
 from collections import deque
 from typing import TypeVar, Callable, Deque
 
@@ -10,6 +12,25 @@ def consume_deque(
     function: Callable[[_T], None],
     consume_new_elements: bool = True
 ) -> None:
+    """Pop all the elements of deque and call a function on each of them (collectively, consume them).
+
+    During the process, this function can push new elements onto the deque (extending it to the
+    right). Depending on the arguments, such "new" elements may or may not be consumed alongside
+    the old ones.
+
+    Parameters:
+        d                    -- the deque to act on.
+        function             -- the function to call on each of the elements. This function can
+                                append new elements to the deque, but must not touch existing
+                                elements or append any elements to the left of the deque. That
+                                is, it may call, for instance, `d.append` or `d.extend`, but
+                                must not call `d.appendleft` or `d.extendleft` (this list is
+                                non-exhaustive). If the function modifies the deque in any way
+                                not approved here, no guarantees are given about the correctness
+                                of the algorithm and the behavior of the program.
+        consume_new_elements -- whether or not to consume "new" elements.
+    """
+
     initial_size = len(d)
     i = 0
 
